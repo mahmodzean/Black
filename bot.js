c1a939c734fa0f74bbd7d197ee7f2058d7df17da
mahmodzean <44239884+mahmodzean@users.noreply.github.com>
2018-11-12 20:50:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,17 +3,14 @@ const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
-
 cclient.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`تجربه`,"http://twitch.tv/Death Shop")
+   console.log(----------------);
+      console.log(Desert Bot- Script By : EX Clan);
+        console.log(----------------);
+      console.log(ON ${client.guilds.size} Servers '     Script By : EX Clan ');
+    console.log(----------------);
+  console.log(Logged in as ${client.user.tag}!);
+client.user.setGame(تجربه,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 
- 
- 
-client.login('MzM1Njk3Mjc0ODA4MTcyNTQ1.Dr3a_A.n1VU0VeNiOtjSVh8WTD6H_j8ugE');
+client.login(process.env.BOT_TOKEN);
