2dee6519edab1e68b4f1353f24125e5f0b8e9310
mahmodzean <44239884+mahmodzean@users.noreply.github.com>
2018-11-12 20:06:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
- const prefix = ".";
+ const prefix = "@";
 client.on('ready', () => {
     console.log('I am ready!');
 });
